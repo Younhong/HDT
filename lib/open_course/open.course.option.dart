2458cec548f3bfa0dd999928c879c37a31ddd2a5
Younhong <younhong@kakao.com>
2020-06-05 15:15:23 +0900

    학부별 강의 검색 기능

--- lib/open_course/open.course.option.dart
+++ lib/open_course/open.course.option.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hgt/open_course/dept.course.dart';
 
 class OpenCourseOptionPage extends StatefulWidget{
   final String option;
@@ -10,12 +11,16 @@ class OpenCourseOptionPage extends StatefulWidget{
 class _OpenCourseOptionState extends State<OpenCourseOptionPage> {
   _OpenCourseOptionState();
 
+  List<String> _deptCategory = ['GLS', 'CSEE', 'Law', 'Life Science'];
+  String _selectedDept = "GLS";
+
   @override
   Widget build(BuildContext context) {
     if (widget.option == "Option") {
       return Column(
         children: <Widget>[
-          Text("전체 강좌 조회")
+          Text("전체 강좌 조회"),
+          Text("옵션 없이 전체 강좌 찾아서 쭉 나열")
         ],
       );
     }
@@ -30,6 +35,28 @@ class _OpenCourseOptionState extends State<OpenCourseOptionPage> {
       return Column(
         children: <Widget>[
           Text("교수명 검색")
+        ],
+      );
+    }
+    else if (widget.option == "Dept Name") {
+      return Column(
+        children: <Widget>[
+          Text("학부별 검색"),
+          DropdownButton(
+            hint: Text('Option'),
+            value: _selectedDept,
+            icon: Icon(Icons.arrow_drop_down),
+            onChanged: (String newValue) {
+              setState(() { _selectedDept = newValue;});
+            },
+            items: _deptCategory
+                .map((String category) {
+              return DropdownMenuItem<String>(
+                value: category, child: Text(category),
+              );
+            }).toList(),
+          ),
+          DeptCourse(_selectedDept),
         ],
       );
     }
